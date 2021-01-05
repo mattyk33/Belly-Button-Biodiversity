116e9e384c356ff9d3f64dc807b85fd60d8a529e
Matthew Kukulski <Matthew.Kukulski@du.edu>
2021-01-05 09:26:03 -0700

    initial d3 select variable assignment

---
+++ BellyButton/static/js/app.js
@@ -0,0 +1,14 @@
+// select the user input field
+var dropDownMenu = d3.select("#selDataset");
+
+// select the metadata div
+var demographTable = d3.select("#sample-metadata");
+
+// select the bar chart div
+var barChart = d3.select("#bar");
+
+// select the bubble chart div
+var bubbleChart = d3.select("#bubble");
+
+// select the gauge chart div
+var gaugeChart = d3.select("#gauge");
