721806da174f64cd12b41e3f3e87f4fb119c3fa9
Matthew Kukulski <Matthew.Kukulski@du.edu>
2021-01-05 10:21:11 -0700

    define init function

--- BellyButton/static/js/app.js
+++ BellyButton/static/js/app.js
@@ -12,3 +12,24 @@ var bubbleChart = d3.select("#bubble");
 
 // select the gauge chart div
 var gaugeChart = d3.select("#gauge");
+
+// create the function for the initial data rendering
+function init() {
+
+    resetData();
+
+    // read the data 
+    d3.json("samples.json").then((data) => {
+        var idArray = data.names;
+        // append id data to the dropdwown menu
+        for (var i = 0; i < idArray.length; i++) {
+            dropDownMenu.append("option").text(idArray[i]).property("value");
+        };
+
+        // call the functions to display the data and the plots to the page
+        // buildPlot(data.names[0]);
+        // getData(data.names[0]);
+    });
+}
+
+init();
